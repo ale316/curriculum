8a852a3c4a7b49065316c6ed9f0c483b8ccd4642
Alessandro Marin <amarin@uwaterloo.ca>
2013-03-24 19:44:36 -0400

    Styling + javascript scrolling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+var page	   = $('html, body');
 
+var animTime   = 500;
+
+$('#menu a').on('click', function(e) {
+	e.preventDefault();
+	page.animate({
+		scrollTop: $(e.currentTarget.hash).offset().top
+	}, animTime);
+});
