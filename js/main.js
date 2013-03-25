8c9255b11dfa329f2c9ae794369e4111a92a5c25
Alessandro Marin <amarin@uwaterloo.ca>
2013-03-24 23:19:29 -0400

    More styling, charts

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
+var skills	   = $('#skills');
 var page	   = $('html, body');
 
 var animTime   = 500;
+
+var chartLoaded = false;
 
 $('#menu a').on('click', function(e) {
 	e.preventDefault();
 	page.animate({
 		scrollTop: $(e.currentTarget.hash).offset().top
 	}, animTime);
+});
+
+var loadCharts = function() {
+	var charts = $('.line-chart');
+	charts.each(function(i, chart) {
+		$(chart).children().each(function(i, bar){
+			$(bar).animate({
+				'width': $(bar).attr('data-percent') + "%"
+			}, 800);
+		});
+	});
+};
+
+$(window).scroll(function() {
+	if($(window).scrollTop() >= (skills.offset().top-skills.height())*0.5 && !chartLoaded) {
+		loadCharts();
+		chartLoaded = true;
+	}
 });
